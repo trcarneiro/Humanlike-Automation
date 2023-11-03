bdb4766a81866ad6a7867869eb686b0b02581e27
Thiago Carneiro <trcampos@gmail.com>
2023-11-03 18:59:36 +0000

    Version: 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='botinfrastructure',
-    version='0.1.7D',
+    version='0.1.8',
     packages=find_packages(),
     install_requires=[
         'attrs==23.1.0',
